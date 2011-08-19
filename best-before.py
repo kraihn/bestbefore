a9f21b24a7305670cb96bbf42a6cb12abd5ec256
kraihn <jared@dunesky.com>
2011-08-19 18:05:05 -0400

    Checking parameters for a valid string length before sending to the interpreter.

--- best-before.py
+++ best-before.py
@@ -3,11 +3,14 @@ import sys
 
 def main():
 	for arg in sys.argv[1:]:
-		interpret(arg);
+		if len(arg) > 4:
+			interpret(arg)
+		else:
+			print arg + ' is illegal'
 
 
 def interpret(arg):
-	print arg
+	print arg + ' ' + str(len(arg))
 
 
 if __name__ == '__main__':
