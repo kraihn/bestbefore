64ccb3c9c6c4520f4b75757ebad1ca6d4416cb18
kraihn <jared@dunesky.com>
2011-08-21 22:58:57 -0400

    Project is now ready for testing. Soonest date should be printed. Years before 2000 aren't fully handled.

--- best-before.py
+++ best-before.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 import sys
+import datetime
 from datetime import date
 
 
@@ -21,7 +22,7 @@ def interpret(arg):
 
 		try:
 			year = int(input[0])
-			if year < 2000:
+			if year < 1000:
 				year += 2000
 			dates.append(date(year,int(input[1]),int(input[2])))
 		except: 
@@ -29,7 +30,7 @@ def interpret(arg):
 
 		try:
 			year = int(input[0])
-			if year < 2000:
+			if year < 1000:
 				year += 2000
 			dates.append(date(year,int(input[2]),int(input[1])))
 		except: 
@@ -37,7 +38,7 @@ def interpret(arg):
 
 		try:
 			year = int(input[1])
-			if year < 2000:
+			if year < 1000:
 				year += 2000
 			dates.append(date(year,int(input[0]),int(input[2])))
 		except: 
@@ -45,7 +46,7 @@ def interpret(arg):
 
 		try:
 			year = int(input[1])
-			if year < 2000:
+			if year < 1000:
 				year += 2000
 			dates.append(date(year,int(input[2]),int(input[0])))
 		except: 
@@ -53,7 +54,7 @@ def interpret(arg):
 
 		try:
 			year = int(input[2])
-			if year < 2000:
+			if year < 1000:
 				year += 2000
 			dates.append(date(year,int(input[0]),int(input[1])))
 		except: 
@@ -61,18 +62,27 @@ def interpret(arg):
 
 		try:
 			year = int(input[2])
-			if year < 2000:
+			if year < 1000:
 				year += 2000
 			dates.append(date(year,int(input[1]),int(input[0])))
 		except:
 			pass
             
-            
-            
-		print dates
 		
 		if len(dates) > 0:
-			print dates[0]
+			now = date.today()
+			
+			best = dates[0];
+			count = (dates[0] - now).days
+			
+			for bdate in dates[1:]:
+				days = (bdate - now).days
+				if days < count:
+					best = bdate
+					count = days
+			
+			print best
+				
 		else:
 			print arg + ' is illegal'
 
